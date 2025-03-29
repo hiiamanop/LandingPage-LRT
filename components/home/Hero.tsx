e5eee14ccd81b3e5aa7d7886bc6a5292f066c637
Ahmad Naufal Muzakki <naufalmuzakki895@gmail.com>
2025-03-29 23:34:30 +0700

    fix : add margin top on hero item

--- components/home/Hero.tsx
+++ components/home/Hero.tsx
@@ -1,44 +1,50 @@
 import Image from "next/image";
 import { Input } from "@/components/ui/input";
-import { Search} from "lucide-react"
+import { Search } from "lucide-react";
 
 const Hero = () => {
-    return (
-        <section className="relative min-h-[50vh]">
-            {/* Background Image */}
-            <div className="absolute-inset-0">
-                <Image src="/images/bg-hero.png" alt="LRT Hero Background" fill className="object-cover"/>
-                <div className="absolute inset-0 bg-black/70"/>
-            </div>
+  return (
+    <section className="relative min-h-[50vh]">
+      {/* Background Image */}
+      <div className="absolute-inset-0">
+        <Image
+          src="/images/bg-hero.png"
+          alt="LRT Hero Background"
+          fill
+          className="object-cover"
+        />
+        <div className="absolute inset-0 bg-black/70" />
+      </div>
 
-            {/* Content */}
-            <div className="relative h-[50vh] flex flex-col items-center justify-center px-4">
-                <p className="text-white/90 mb-12">
-                LRT Sumatera Selatan
-                </p>
+      {/* Content */}
+      <div className="relative h-[50vh] flex flex-col items-center justify-center px-4 ">
+        <p className="text-white/90 mb-2 mt-20">LRT Sumatera Selatan</p>
 
-                {/* Main Heading */}
-                <h1 className="font-poppins font-bold text-white text-center
+        {/* Main Heading */}
+        <h1
+          className="font-poppins font-bold text-white text-center
                 text-3xl md:text-4xl lg:text;[48px]
                 leading-[150%] tracking-[0.5px]
-                max-w-3xl mb-12">
-                    Solusi Transportasi Modern
-                    <br/>
-                    untuk Mobilitas Lebih Mudah!
-                </h1>
+                max-w-3xl mb-6"
+        >
+          Solusi Transportasi Modern
+          <br />
+          untuk Mobilitas Lebih Mudah!
+        </h1>
 
-                {/* Search Bar */}
-                <div className="
+        {/* Search Bar */}
+        <div
+          className="
                 relative 
                 w-full max-w-md 
                 flex items-center
-                ">
-                <Input
-                    type="text"
-                    placeholder="Jadwal kereta terdekat?"
-                    className="
+                "
+        >
+          <Input
+            type="text"
+            placeholder="Jadwal kereta terdekat?"
+            className="
                     pl-4 pr-12
-                    py-6
                     bg-white/90 
                     backdrop-blur-sm
                     text-gray-800
@@ -48,9 +54,10 @@ const Hero = () => {
                     focus:ring-2 focus:ring-primary
                     hidden md:block
                     "
-                />
-                {/* Search Icon */}
-                <button className="
+          />
+          {/* Search Icon */}
+          <button
+            className="
                     md:absolute md:right-4
                     p-3 md:p-2
                     bg-primary md:bg-transparent
@@ -58,14 +65,14 @@ const Hero = () => {
                     text-white md:text-gray-600
                     hover:bg-primary/90 md:hover:bg-transparent
                     transition-colors
-                ">
-                    <Search className="w-6 h-6" />
-                </button>
-                </div>
-            </div>
+                "
+          >
+            <Search className="w-6 h-6" />
+          </button>
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
-
+export default Hero;
